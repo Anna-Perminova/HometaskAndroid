70dcc871537409e66660e404607ff8f08c010c8a
Annushka <annaperminova888@gmail.com>
2021-12-12 15:55:11 +0300

    Hometask14

---
+++ app/src/main/java/com/example/myapplication14/Heir.kt
@@ -0,0 +1,32 @@
+package com.example.myapplication14
+
+open class Heir : User {
+val name:String
+    constructor(
+        firstNum: Int,
+        secondNum: Int,
+        thirdNum: Int,
+        forthNum: Int,
+        fifthNub: Int,
+        age: Int,
+        name:String
+    ) : super(firstNum, secondNum, thirdNum, forthNum, fifthNub, age) {
+
+this.name =name
+}
+
+
+    override fun toString(): String {
+        return "Heir(name='$name')"
+    }
+
+
+}
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/example/myapplication14/Util.kt
@@ -0,0 +1,27 @@
+package com.example.myapplication14
+
+open class Util {
+    val sum: Heir
+
+    constructor(sum: Heir) {
+        this.sum = sum
+    }
+
+    fun summ(): Int {
+    return sum.firstNum+sum.secondNum+sum.thirdNum+sum.forthNum+sum.fifthNub+sum.age
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
